dbc37d121c18a7b3486d3e5e90d5d0a4f717e9a4
linux_china <libing.chen@gmail.com>
2019-09-24 11:22:32 -0700

    moved

--- src/test/java/org/mvnsearch/boot/gossip/seed1/SeedServer1.java
+++ src/test/java/org/mvnsearch/boot/gossip/seed1/SeedServer1.java
@@ -1,22 +1,25 @@
-package org.mvnsearch.boot.gossip;
+package org.mvnsearch.boot.gossip.seed1;
 
 import io.scalecube.cluster.ClusterMessageHandler;
 import io.scalecube.cluster.membership.MembershipEvent;
 import io.scalecube.cluster.transport.api.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 /**
- * seed server 2
+ * seed server 1
  *
  * @author linux_china
  */
 @SpringBootApplication
-public class SeedServer2 {
+public class SeedServer1 {
+    private Logger log = LoggerFactory.getLogger(SeedServer1.class);
 
     public static void main(String[] args) throws InterruptedException {
-        SpringApplication.run(SeedServer2.class, args);
+        SpringApplication.run(SeedServer1.class, args);
     }
 
     @Bean
@@ -29,12 +32,16 @@ public class SeedServer2 {
 
             @Override
             public void onGossip(Message gossip) {
-
+                System.out.println(gossip);
             }
 
             @Override
             public void onMembershipEvent(MembershipEvent event) {
-
+                if (event.isAdded()) {
+                    log.info("Member joined: " + event.member().address());
+                } else if (event.isRemoved()) {
+                    log.info("Member left: " + event.member().address());
+                }
             }
         };
     }
--- src/test/java/org/mvnsearch/boot/gossip/seed2/SeedServer2.java
+++ src/test/java/org/mvnsearch/boot/gossip/seed2/SeedServer2.java
@@ -1,22 +1,25 @@
-package org.mvnsearch.boot.gossip;
+package org.mvnsearch.boot.gossip.seed2;
 
 import io.scalecube.cluster.ClusterMessageHandler;
 import io.scalecube.cluster.membership.MembershipEvent;
 import io.scalecube.cluster.transport.api.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 /**
- * seed server 1
+ * seed server 2
  *
  * @author linux_china
  */
 @SpringBootApplication
-public class SeedServer1 {
+public class SeedServer2 {
+    private Logger log = LoggerFactory.getLogger(SeedServer2.class);
 
     public static void main(String[] args) throws InterruptedException {
-        SpringApplication.run(SeedServer1.class, args);
+        SpringApplication.run(SeedServer2.class, args);
     }
 
     @Bean
@@ -29,12 +32,16 @@ public class SeedServer1 {
 
             @Override
             public void onGossip(Message gossip) {
-
+                System.out.println(gossip);
             }
 
             @Override
             public void onMembershipEvent(MembershipEvent event) {
-
+                if (event.isAdded()) {
+                    log.info("Member joined: " + event.member().address());
+                } else if (event.isRemoved()) {
+                    log.info("Member left: " + event.member().address());
+                }
             }
         };
     }
